a1106c38f23213a7a8b5e33f3feb54f2cba8a0e1
Kevin Deyne <kdeyne@accurate.com>
2020-09-17 10:27:38 -0700

    Added old version of GSON + Timezone settings

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Los_Angeles"));
 		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("Application started on port 8080 ...");
 	}
 
 }
